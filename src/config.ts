44b6bcc2cacf99ce538fdf068b500d16df2a0ff1
ErrorTeaPot <gabin.lesaout@etudiant.univ-rennes.fr>
2024-09-16 16:27:32 +0200

    Change social medias

--- src/config.ts
+++ src/config.ts
@@ -27,7 +27,7 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/ErrorTeaPot",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
@@ -35,19 +35,19 @@ export const SOCIALS: SocialObjects = [
     name: "Facebook",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Facebook`,
-    active: true,
+    active: false,
   },
   {
     name: "Instagram",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Instagram`,
-    active: true,
+    active: false,
   },
   {
     name: "LinkedIn",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on LinkedIn`,
-    active: true,
+    active: false,
   },
   {
     name: "Mail",
